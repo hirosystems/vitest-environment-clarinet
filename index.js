1bd8548f9daafae2a2f71f55c6fed08955f419a9
Hugo Caillard <911307+hugocaillard@users.noreply.github.com>
2023-10-03 15:03:55 +0200

    feat: handle the cost options

--- index.js
+++ index.js
@@ -21,10 +21,8 @@ export default {
     const covFilename = options.clarinet.coverageFilename || "lcov.info";
     const costsFilename = options.clarinet.costsFilename || "costs-reports.json";
 
-    if (options.clarinet.coverage) {
-      if (fs.existsSync(covFilename)) fs.rmSync(covFilename);
-      if (fs.existsSync(costsFilename)) fs.rmSync(costsFilename);
-    }
+    if (options.clarinet.coverage && fs.existsSync(covFilename)) fs.rmSync(covFilename);
+    if (options.clarinet.costs && fs.existsSync(costsFilename)) fs.rmSync(costsFilename);
 
     const clarityVM = await initVM();
 
@@ -38,8 +36,10 @@ export default {
       async teardown() {
         if (options.clarinet.coverage) {
           fs.writeFileSync(covFilename, global.coverageReports.join("\n"));
+        }
+
+        if (options.clarinet.costs) {
           try {
-            /** @type any[] */
             const costs = global.costsReports.map((r) => JSON.parse(r)).flat();
             fs.writeFileSync(costsFilename, JSON.stringify(costs, null, 2));
           } catch (e) {
